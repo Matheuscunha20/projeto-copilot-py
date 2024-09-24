db43eda7b6e779ee2cfc8a1f1be8a74da27e4b0d
Matheuscunha20 <matheu20pereira15@gmail.com>
2024-09-24 11:10:54 -0300

    Create concatenado.py

---
+++ concatenado.py
@@ -0,0 +1,10 @@
+# Dados
+nome = "Ana"
+idade = 25
+cidade = "São Paulo"
+
+# Concatenando dados
+mensagem = "Olá, meu nome é " + nome + ". Eu tenho " + str(idade) + " anos e moro em " + cidade + "."
+
+# Exibindo a mensagem
+print(mensagem)
