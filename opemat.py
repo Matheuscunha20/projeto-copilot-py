d50b8fd8c5756b3f3587e410cb4376e0a3c1baef
Matheuscunha20 <matheu20pereira15@gmail.com>
2024-09-24 11:11:27 -0300

    Create opemat.py

---
+++ opemat.py
@@ -0,0 +1,15 @@
+# Definindo os números
+a = 10
+b = 5
+
+# Operações matemáticas
+soma = a + b           # Adição
+subtracao = a - b       # Subtração
+multiplicacao = a * b   # Multiplicação
+divisao = a / b         # Divisão
+
+# Exibindo os resultados
+print("Soma:", soma)
+print("Subtração:", subtracao)
+print("Multiplicação:", multiplicacao)
+print("Divisão:", divisao)
